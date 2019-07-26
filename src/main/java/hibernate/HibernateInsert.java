d7f6042d8cbeaad51350d3ecacfea8908583fc82
Lord-Fifth <aditya.suresh1504@gmail.com>
2019-07-26 15:07:24 +0530

    Sample Select

--- src/main/java/hibernate/HibernateInsert.java
+++ src/main/java/hibernate/HibernateInsert.java
@@ -1,5 +1,7 @@
 package hibernate;
 
+import java.util.List;
+
 import org.hibernate.*;
 
 public class HibernateInsert {
@@ -7,13 +9,15 @@ public class HibernateInsert {
     public static void main(String[] args)
     {
         Session session = HibernateUtil.getSessionFactory().openSession();
+        Session session1 = HibernateUtil.getSessionFactory().openSession();
+        Session session2 = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
         
         //Add new Employee object
         EmployeeEntity emp = new EmployeeEntity();
-        emp.setNo(101);
+        emp.setNo(102);
         emp.setBdate("1997-09-21");
-        emp.setFname("Bla");
+        emp.setFname("Loo");
         emp.setLname("Bla");
         emp.setGen("F");	
         emp.setHdate("2015-09-09");
@@ -23,6 +27,21 @@ public class HibernateInsert {
  
         //Commit the transaction
         session.getTransaction().commit();
+        
+        session1.beginTransaction();
+        EmployeeEntity empres=new EmployeeEntity();
+        empres=(EmployeeEntity) session1.load(EmployeeEntity.class, 101);
+        System.out.println("Loading data");
+        System.out.println(empres.getFname()+" "+empres.getLname());
+        session2.beginTransaction();
+        //EmployeeEntity empres2=new EmployeeEntity();
+        Query query=session2.createQuery("from EmployeeEntity");
+        List<EmployeeEntity> list=query.list();
+        for(EmployeeEntity employeeEntity : list){
+        	System.out.println(employeeEntity.getFname()+" "+employeeEntity.getLname());
+        }
+        //empres2=(EmployeeEntity) session2.get(EmployeeEntity.class, 101);
+        session1.close();	
         
         HibernateUtil.shutdown();
     }
